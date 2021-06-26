1744a69e004f7d8f31800b74451d404db8a5941a
foolish-pine <59694183+foolish-pine@users.noreply.github.com>
2021-06-26 10:50:02 +0900

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
-  const onClickButton = () => {
-    alert();
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(true);
+
+  const onClickCountUp = () => {
+    setNum(num + 1);
   };
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
   return (
     <>
       <h1 style={{ color: "red" }}>test</h1>
-      <p>p</p>
-      <button onClick={onClickButton}>ボタン</button>
+      <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
+      <ColorfulMessage color="pink">元気です！</ColorfulMessage>
+      <button onClick={() => onClickCountUp()}>カウントアップ</button>
+      <button onClick={() => onClickSwitchShowFlag()}>on/off</button>
+      <p>{num}</p>
+      {faceShowFlag && <p>(*^-^*)</p>}
     </>
   );
 };
